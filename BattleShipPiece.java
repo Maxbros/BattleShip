1f886e7ca345880ce9e378f940e87846b09aa4e2
Maxbros <maxbros90@gmail.com>
2017-02-27 18:52:45 -0600

    Add files via upload

--- BattleShipPiece.java
+++ BattleShipPiece.java
@@ -6,8 +6,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by SALAH_M on 20/02/2017.
+ * Created by MAX on 20/02/2017.
  */
+
+
+enum TypeShip { UNKNOW,  Portaviones, Acorazado, Patrulla;
+    public static TypeShip fromInt(int dimension)
+    {
+        TypeShip theType = TypeShip.UNKNOW;
+
+        switch (dimension)
+        {
+            case 5:
+                theType = TypeShip.Portaviones;
+            case 3:
+                theType = TypeShip.Acorazado;
+            case 1:
+                theType = TypeShip.Patrulla;
+        }
+
+        return theType;
+};
+
 public class BattleShipPiece {
     boolean isAlive = false;
     PieceColor color = PieceColor.COLOR_UNKNOWN;
